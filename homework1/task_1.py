a11c5135618bbf690b45d355326849003d57b262
AbuOrstho <susurkievabdulaziz6@gmail.com>
2022-11-06 14:50:51 +0300

    Signed-off-by: AbuOrstho <susurkievabdulaziz6@gmail.com>

---
+++ homework1/task_1.py
@@ -0,0 +1,10 @@
+def ord_in_ascii(a):
+    return ord(a)
+
+
+assert(ord_in_ascii('A')) == 65
+assert(ord_in_ascii('B')) == 66
+assert(ord_in_ascii('C')) == 67
+assert(ord_in_ascii('Z')) == 90
+assert(ord_in_ascii('a')) == 97
+assert(ord_in_ascii('z')) == 122
