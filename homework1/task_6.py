b6579719ef3e6bfcfcfa09f7d3ddfe0b7a8c74bf
AbuOrstho <susurkievabdulaziz6@gmail.com>
2022-11-14 18:52:28 +0300

    Signed-off-by: AbuOrstho <susurkievabdulaziz6@gmail.com>

--- homework1/task_6.py
+++ homework1/task_6.py
@@ -1,18 +1,16 @@
 def unpack(f):
     x = str(f)
     s = '1234567890'
-    n = '['
+    n = ''
     for i in x:
         if i in s:
-            n += i + ' ,'
-    return n[:-3] + ']'
+            n += i + ','
+    L = [n[:-1]]
+    return list(map(int, L[0].split(',')))
 
 
-
+print(unpack( [0,0,[[1,2],-1,-2],5,6] ))
 assert(unpack( [[1,3],4,5]  )) == [1,3,4,5]
-res.clear()
 assert(unpack( [0,0,[[1,2],-1,-2],5,6] )) == [0, 0, 1, 2, -1, -2, 5, 6]
-res.clear()
 assert(unpack( [[[[[[[1]]]]]]] )) == [1]
-res.clear()
 assert(unpack( [1,[2,[3,[4,[5,[6,[7,[8,[9]]]]]]]]] )) == [1,2,3,4,5,6,7,8,9]
