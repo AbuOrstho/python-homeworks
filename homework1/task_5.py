93355474618347c5cea574b5a757f1b90cc91cc5
AbuOrstho <susurkievabdulaziz6@gmail.com>
2022-11-14 21:22:58 +0300

    Signed-off-by: AbuOrstho <susurkievabdulaziz6@gmail.com>

--- homework1/task_5.py
+++ homework1/task_5.py
@@ -1,13 +1,15 @@
-def reverse(x, a=0):
+def reverse_int(x, a=0):
     f = 0
     if x == 0:
         f = a
     else:
-        f = reverse(x // 10, a * 10 + x % 10)
+        f = reverse_int(x // 10, a * 10 + x % 10)
     return f
-assert (int(reverse(1000))) == 1
-assert (int(reverse(142522))) == 225241
-assert (int(reverse(104))) == 401
-assert (int(reverse(10001))) == 10001
-assert (int(reverse(111111111))) == 111111111
-assert (int(reverse(101010101010000000000000000))) == 10101010101
+
+print(reverse_int(1111111))
+assert(reverse_int(1000)) == 1
+assert(reverse_int(142522)) == 225241
+assert(reverse_int(104)) == 401
+assert(reverse_int(10001)) == 10001
+assert(reverse_int(111111111)) == 111111111
+assert(reverse_int(101010101010000000000000000)) == 10101010101
